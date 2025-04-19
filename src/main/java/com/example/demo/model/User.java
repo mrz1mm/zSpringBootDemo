de4e02a0f20f0d3d9fc3431b3a543e19ece3bd44
Simone Attanasio <161967392+mrz1mm@users.noreply.github.com>
2025-04-19 14:25:16 +0200

    Fase 4: Creazione delle Entità JPA e dei Repository Spring Data

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,39 @@
+package com.example.demo.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+    @Id
+    @Column(name = "username", length = 50, nullable = false)
+    private String username;
+
+    @Column(name = "password", length = 500, nullable = false)
+    private String password;
+
+    @Column(name = "enabled", nullable = false)
+    private boolean enabled;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "authorities", joinColumns = @JoinColumn(name = "username"))
+    @Column(name = "authority", nullable = false)
+    private Set<String> authorities = new HashSet<>();
+}
